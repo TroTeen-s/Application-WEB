c53a92805e33637c4fd7d3a98337b98283922e11
lyeslimani <lslimani@myges.fr>
2022-04-11 00:24:23 +0200

    migrations reste bdd

---
+++ database/migrations/2022_04_10_215355_create-repair_centers.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('repair_centers', function (Blueprint $table) {
+            $table->id();
+            $table->string('road', 200);
+            $table->string('city', 100);
+            $table->string('postal_code', 5);
+            $table->string('phone_number', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('repair_centers');
+    }
+};
