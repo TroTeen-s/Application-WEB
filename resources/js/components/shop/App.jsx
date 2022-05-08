89b90e94bb07d18a69a19e1139b079640474c84d
MTthoas <matthias.pecquery@gmail.com>
2022-05-08 17:37:04 +0200

    shop modification

---
+++ resources/js/components/shop/App.jsx
@@ -0,0 +1,10 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import {BrowserRouter} from 'react-router-dom'
+const App = () => {
+  return(
+     <Home/>
+  )
+};
+
+export default App;
---
+++ resources/js/components/shop/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Products from "../components/Products";
+
+
+const Home = () => {
+  return (
+    <div className="container">
+
+      <Products/>
+
+    </div>
+  );
+};
+
+export default Home;
