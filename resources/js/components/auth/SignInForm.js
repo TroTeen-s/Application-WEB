c3a69f289a4dcd54fc75b372dd27968faa7c35ad
MTthoas <matthias.pecquery@gmail.com>
2022-05-26 02:13:36 +0200

    bug corrigé

--- resources/js/components/auth/SignInForm.js
+++ resources/js/components/auth/SignInForm.js
@@ -143,18 +143,12 @@ export default function SignInForm() {
                         </h2>
                     </div>
                     <Box class="mt-8 space-y-6" component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                        <input type="hidden" name="remember" value="true" />
-                        {/* <div class="rounded-md shadow-sm -space-y-px">
-        <div>
-          <label for="email-address" class="sr-only">Email address</label>
-          <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"/>
-        </div>
-        <div>
-          <label for="password" class="sr-only">Password</label>
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"/>
-        </div>
-      </div> */}
-
+                    {isFormInvalid ?
+                        <div className=" flex items-center justify-between text-orange-300">
+                            <span className="m-0 p-0 text-center">Identifiants incorrectes</span>
+                            <span className="cursor-pointer text-center" onClick={removeError}>X</span>
+                        </div> :
+                        <></>}
                         <StyledTextField
 
                             margin="normal"
