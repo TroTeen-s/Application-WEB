6bc0e1fc2ca17a7f58f869e72c126bdf4d0648b4
MTthoas <matthias.pecquery@gmail.com>
2022-05-26 14:42:05 +0200

    Product debug

--- resources/js/components/shop/components/Details.jsx
+++ resources/js/components/shop/components/Details.jsx
@@ -25,41 +25,30 @@ const Product = () => {
           </div>
         </NavLink>
       </div> */}
-      <div className="">
-        <div className="flex items-center p-2 lg:p-5 mt-5 overflow-hidden relative">
-          <div className="w-full max-w-6xl rounded bg-white-background p-20 pt-5 pb-5 lg:p-20 mx-auto text-gray-800 relative md:text-left">
-              <div className="md:flex items-center -mx-12">
-                  <div className="w-full md:w-1/2 px-10 mb-10 md:mb-0">
-                      <div className="relative">
-                          <img src="https://pngimg.com/uploads/raincoat/raincoat_PNG53.png" className="w-full relative z-10" alt=""/>
-                      </div>
-                  </div>
-                  <div className="w-full md:w-1/2 px-10">
-                      <div className="mb-10">
-                          <h1 className="font-bold uppercase text-2xl mb-5">Mens's Ragged <br/> Waterproof Jacket</h1>
-                          <p className="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing, elit. Eos, voluptatum dolorum! Laborum blanditiis consequatur, voluptates, sint enim fugiat saepe, dolor fugit, magnam explicabo eaque quas id quo porro dolorum facilis... 
-                          {/* <a href="#" className="opacity-50 text-gray-900 hover:opacity-100 inline-block text-xs leading-none border-b border-gray-900">MORE <i className="mdi mdi-arrow-right"></i></a>*/}
-                          </p> 
-                      </div>
-                      <div>
-                          <div className="align-bottom">
-                              <span className="font-bold text-5xl leading-none align-baseline">59.00</span>
-                              <span className="font-bold text-5xl leading-none align-baseline">€</span>
+      
+    <div class="py-16 bg-green-50 mt-12 pt-5 ">
+        <div class="container m-auto px-6 space-y-8 md:px-12 lg:px-20">
+            <div class="gap-16 md:text-left md:flex mb-5">
+                <div class="order-last mb-6 space-y-6 md:mb-0 md:w-7/12 lg:w-6/12 mr-6 pr-12">
+                <h1 className="font-bold uppercase text-3xl mb-5">Mens's Ragged <br/> Waterproof Jacket</h1>
+                    <p class="text-lg text-gray-600">Be part of millions people around the world using tailus in modern User Interfaces.</p>
+                    <div className="align-bottom">
+                              <span className="font-bold text-5xl leading-none align-baseline pt-2">59.00</span>
+                              <span className="font-bold text-5xl leading-none align-baseline pl-2">€</span>
             
                           </div>
-                          
-                          <div className="align-bottom mt-5">
-                              <button className="bg-orange-300 opacity-75 hover:opacity-100 text-black-trot rounded-full px-10 py-2 font-semibold">
-                                <i className="mdi mdi-cart -ml-2 mr-2"></i> 
+                    <div class="flex flex-wrap justify-center gap-4 lg:gap-6 md:justify-start">
+                        <button className="bg-orange-300 opacity-75 hover:opacity-100 text-black-trot px-10 py-2 font-semibold">
+                            <i className="mdi mdi-cart -ml-2 mr-2"></i> 
                                 ADD TO CART
-                              </button>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </div>
+                        </button>
+                    </div>
+                </div>
+                <img src="https://pngimg.com/uploads/raincoat/raincoat_PNG53.png" width="400" height="400" class="md:w-5/12 m-auto" loading="lazy" alt="mobility_illustration"/>
+            </div>
+        </div>
+    </div> 
+        
 
     <div class="py-16">  
     <div class="container px-2 text-gray-500 md:px-2 xl:px-12">
