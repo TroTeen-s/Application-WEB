102c83d91fa710ebc856e4a64411c5b16522defb
Prumme <aurelien23.p@gmail.com>
2022-05-26 23:28:21 +0200

    svg weather icon

---
+++ resources/js/components/header/components/Weather.js
@@ -0,0 +1,18 @@
+import Cloudy_day from "../../../../data/weather/Cloudy_day";
+import Cloudy_night from "../../../../data/weather/Cloudy_night";
+import Cloudy from "../../../../data/weather/cloudy";
+import Sunny from "../../../../data/weather/Sunny";
+import Night from "../../../../data/weather/Night";
+import Rainy_day from "../../../../data/weather/Rainy_day";
+import Rainy from "../../../../data/weather/Rainy";
+import Snowy from "../../../../data/weather/Snowy";
+import Thunder from "../../../../data/weather/thunder";
+
+const Weather = () => {
+
+    return (
+        <Thunder />
+    );
+};
+
+export default Weather;
