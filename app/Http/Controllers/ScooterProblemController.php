80695daa43ed10fc507054139549aaa481b224e1
Prumme <aurelien23.p@gmail.com>
2022-06-23 19:09:50 +0200

    error log

--- app/Http/Controllers/ScooterProblemController.php
+++ app/Http/Controllers/ScooterProblemController.php
@@ -33,7 +33,7 @@ class ScooterProblemController extends Controller
         $scooter_id = $request->header('scooter_id');
         $reporter_email = $request->header('reporter_email');
 
-        return response()->json(array('data' => $scooter_id));
+        return response()->json(array('data' => $scooter_id, "request" => $request, 'description' => $description));
 
         $probleme = new ScooterProblem([
             'description' => $description,
