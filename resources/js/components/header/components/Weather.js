089517f02dce6edf0aff8a7e2bc9c7349df95cc4
lyeslimani <lslimani@myges.fr>
2022-07-14 21:46:29 +0200

    fixup

--- resources/js/components/header/components/Weather.js
+++ resources/js/components/header/components/Weather.js
@@ -94,7 +94,7 @@ const Weather = () => {
                     );
 
                 default:
-                    return (<></>);
+                    return (<>{infos.description}</>);
             }
         }
 
